56c3534fcb36c837c72fad93a759bb46194f7a67
Pawkooo13 <strzalecki9@wp.pl>
2025-08-31 18:40:52 +0200

    Added config file with paths to data and annotations

---
+++ src/config.py
@@ -0,0 +1,21 @@
+DATA_DIR = 'data/' 
+
+# SKIMMIA 
+SKIMMIA_IMAGES_TRAIN = 'data/skimmia/images/train' 
+SKIMMIA_IMAGES_VALID = 'data/skimmia/images/valid' 
+SKIMMIA_IMAGES_TEST = 'data/skimmia/images/test' 
+
+SKIMMIA_ANNOTATIONS_TRAIN = 'data/skimmia/annotations/annotations_train.csv' 
+SKIMMIA_ANNOTATIONS_VALID = 'data/skimmia/annotations/annotations_valid.csv' 
+SKIMMIA_ANNOTATIONS_TEST = 'data/skimmia/annotations/annotations_test.csv'
+
+# VISEM
+VISEM_IMAGES_TRAIN = 'data/visem/images/train' 
+VISEM_IMAGES_VALID = 'data/visem/images/valid' 
+VISEM_IMAGES_TEST = 'data/visem/images/test' 
+
+VISEM_ANNOTATIONS_TRAIN = 'data/visem/annotations/annotations_train.csv' 
+VISEM_ANNOTATIONS_VALID = 'data/visem/annotations/annotations_valid.csv' 
+VISEM_ANNOTATIONS_TEST = 'data/visem/annotations/annotations_test.csv'
+
+PLOTS_DIR = 'plots/'
