7d611e29820d53a30596ea7a0432f148cc34d1d3
Pawkooo13 <strzalecki9@wp.pl>
2025-08-31 20:06:42 +0200

    Added IoU fucntion and moved paths to paths.py

--- src/config.py
+++ src/config.py
@@ -1,21 +1,39 @@
-DATA_DIR = 'data/' 
+def iou(box1, box2):
+    """
+    Compute Intersection Over Union (IoU) between two bounding boxes
+    in (x_center, y_center, w, h) format.
+    """
+    # Convert (x_center, y_center, w, h) to (x1, y1, x2, y2)
+    def to_corners(box):
+        x_center, y_center, w, h = box
+        x1 = x_center - w / 2
+        y1 = y_center - h / 2
+        x2 = x_center + w / 2
+        y2 = y_center + h / 2
+        return x1, y1, x2, y2
 
-# SKIMMIA 
-SKIMMIA_IMAGES_TRAIN = 'data/skimmia/images/train' 
-SKIMMIA_IMAGES_VALID = 'data/skimmia/images/valid' 
-SKIMMIA_IMAGES_TEST = 'data/skimmia/images/test' 
+    x1_1, y1_1, x2_1, y2_1 = to_corners(box1)
+    x1_2, y1_2, x2_2, y2_2 = to_corners(box2)
 
-SKIMMIA_ANNOTATIONS_TRAIN = 'data/skimmia/annotations/annotations_train.csv' 
-SKIMMIA_ANNOTATIONS_VALID = 'data/skimmia/annotations/annotations_valid.csv' 
-SKIMMIA_ANNOTATIONS_TEST = 'data/skimmia/annotations/annotations_test.csv'
+    # Intersection coordinates
+    xi1 = max(x1_1, x1_2)
+    yi1 = max(y1_1, y1_2)
+    xi2 = min(x2_1, x2_2)
+    yi2 = min(y2_1, y2_2)
 
-# VISEM
-VISEM_IMAGES_TRAIN = 'data/visem/images/train' 
-VISEM_IMAGES_VALID = 'data/visem/images/valid' 
-VISEM_IMAGES_TEST = 'data/visem/images/test' 
+    # Intersection area
+    inter_width = max(0, xi2 - xi1)
+    inter_height = max(0, yi2 - yi1)
+    inter_area = inter_width * inter_height
 
-VISEM_ANNOTATIONS_TRAIN = 'data/visem/annotations/annotations_train.csv' 
-VISEM_ANNOTATIONS_VALID = 'data/visem/annotations/annotations_valid.csv' 
-VISEM_ANNOTATIONS_TEST = 'data/visem/annotations/annotations_test.csv'
+    # Areas of each box
+    box1_area = (x2_1 - x1_1) * (y2_1 - y1_1)
+    box2_area = (x2_2 - x1_2) * (y2_2 - y1_2)
 
-PLOTS_DIR = 'plots/'
+    # Union area
+    union_area = box1_area + box2_area - inter_area
+
+    # IoU
+    if union_area == 0:
+        return 0.0
+    return inter_area / union_area
--- src/train.py
+++ src/train.py
@@ -9,7 +9,9 @@ from config import (
 )
 
 def load_data(images_dir, annotations_file):
-    ''' Load images and annotations from the specified directories and files '''
+    """ 
+    Load images and annotations from the specified directories and files.
+    """
     annotations = pd.read_csv(annotations_file)
     image_files = set(os.listdir(images_dir))
 
@@ -25,6 +27,46 @@ def load_data(images_dir, annotations_file):
             images.append(img)
 
     return images, annotations
+
+def iou(box1, box2):
+    """
+    Compute Intersection Over Union (IoU) between two bounding boxes
+    in (x_center, y_center, w, h) format.
+    """
+    # Convert (x_center, y_center, w, h) to (x1, y1, x2, y2)
+    def to_corners(box):
+        x_center, y_center, w, h = box
+        x1 = x_center - w / 2
+        y1 = y_center - h / 2
+        x2 = x_center + w / 2
+        y2 = y_center + h / 2
+        return x1, y1, x2, y2
+
+    x1_1, y1_1, x2_1, y2_1 = to_corners(box1)
+    x1_2, y1_2, x2_2, y2_2 = to_corners(box2)
+
+    # Intersection coordinates
+    xi1 = max(x1_1, x1_2)
+    yi1 = max(y1_1, y1_2)
+    xi2 = min(x2_1, x2_2)
+    yi2 = min(y2_1, y2_2)
+
+    # Intersection area
+    inter_width = max(0, xi2 - xi1)
+    inter_height = max(0, yi2 - yi1)
+    inter_area = inter_width * inter_height
+
+    # Areas of each box
+    box1_area = (x2_1 - x1_1) * (y2_1 - y1_1)
+    box2_area = (x2_2 - x1_2) * (y2_2 - y1_2)
+
+    # Union area
+    union_area = box1_area + box2_area - inter_area
+
+    # IoU
+    if union_area == 0:
+        return 0.0
+    return inter_area / union_area
 
 def main():
     skimmia_images, skimmia_annotations = load_data(SKIMMIA_IMAGES_TRAIN, SKIMMIA_ANNOTATIONS_TRAIN)
